8134195dbf9454ec61e5bef62dcc0760b1f16886
IshaanJolly <ijolly1997@gmail.com>
2026-05-06 11:41:50 +0100

    feat: small change

--- scripts/install_pyproj.sh
+++ scripts/install_pyproj.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
- 
+
+python -m ensurepip
+python -m pip install --upgrade pip
+
 python -m pip download pyproj --only-binary :all: --platform macosx_14_0_arm64 --python-version 3.11 --no-deps -d /tmp/pyproj_wheel
- 
-mv /tmp/pyproj_wheel/pyproj-*macosx_14_0_arm64.whl /tmp/pyproj_wheel/pyproj-macosx_11_0_arm64.whl
+
+mv /tmp/pyproj_wheel/pyproj-3.7.2-cp311-cp311-macosx_14_0_arm64.whl /tmp/pyproj_wheel/pyproj-3.7.2-cp311-cp311-macosx_11_0_arm64.whl
+python -m pip install /tmp/pyproj_wheel/pyproj-3.7.2-cp311-cp311-macosx_11_0_arm64.whl --force-reinstall --no-deps
  
 python -m pip install /tmp/pyproj_wheel/pyproj-macosx_11_0_arm64.whl --force-reinstall --no-deps
 python -m pip install certifi
